4135a3488ab07c80d26c2c3d41aa380df9714419
amaguire-undp <andrew@happysoft.dev>
2025-02-04 19:32:03 +0200

    add env parsing

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
+import react from '@vitejs/plugin-react';
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig(({ command, mode }) => {
-  const externalEnvs = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react(), eslint()],
     define: {
-      'process.env': externalEnvs,
+      'process.env': env,
     },
     build: {
       outDir: 'build',
@@ -23,6 +23,7 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     server: {
+      port: Number.parseInt(env.VITE_PORT || '5173', 10),
       cors: {
         origin: '*',
         methods: ['GET'],
