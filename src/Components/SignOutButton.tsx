6cf8d4408ae93aacad54990536e3fb233ee3184b
andrew-maguire <andrew@happysoft.dev>
2025-04-23 16:59:00 +0300

    add header items

--- src/Components/SignOutButton.tsx
+++ src/Components/SignOutButton.tsx
@@ -144,6 +144,22 @@ export function SignOutButton(props: Props) {
       ),
     },
     {
+      key: 'sprints',
+      label: (
+        <NavLink
+          to='/my-sprints'
+          style={{
+            fontFamily: 'var(--fontFamily)',      
+            fontSize: '1.25rem',
+            textTransform: 'none',
+            padding: '0.75rem',
+          }}
+        >
+          My Sprints
+        </NavLink>
+      ),
+    },  
+    {
       key: 'profile',
       label: (
         <button
